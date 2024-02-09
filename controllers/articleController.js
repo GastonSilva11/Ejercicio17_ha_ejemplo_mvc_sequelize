96df3efc14006590bdb186abb573d9bd4c9a8980
DViergutz <d.viergutz@hotmail.de>
2024-02-09 10:13:37 -0300

    article routes + controllers

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -2,23 +2,46 @@ const { Article } = require("../models");
 
 // Display a listing of the resource.
 function index(req, res) {
+  // Esta será la página principal del proyecto.
   res.render("article");
 }
 
 // Display the specified resource.
-async function show(req, res) {}
+async function show(req, res) {
+  // Aquí necesitaremos los datos del articulo al que se le ha hecho clic.
+  // La info de qué articulo necesitamos debería estar presente como parte de la URL (req.params)
+  const id = req.params.id;
+  res.render(`article/${id}`);
+}
 
 // Show the form for creating a new resource
-async function create(req, res) {}
+async function create(req, res) {
+  // Esta vista tendrá un formulario que será el encargado de recibir los datos para crear
+  // un nuevo artículo.
+  res.render("create");
+}
 
 // Store a newly created resource in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  // - Recibir la información que proviene del formulario (req.body)
+  // - Crear el artículo en la BBDD con todos los datos necesarios
+  // - Redireccionar (redirect) la navegación hacia `[GET] /admin`
+  res.redirect("admin");
+}
 
 // Show the form for editing the specified resource.
-async function edit(req, res) {}
+async function edit(req, res) {
+  // Esta vista tendrá un formulario con los datos precargados del artículo presente en la URL (params).
+  res.render("edit");
+}
 
 // Update the specified resource in storage.
-async function update(req, res) {}
+async function update(req, res) {
+  //   - Recibir la información que proviene del formulario (req.body)
+  // - Hacer un update el artículo mencionado.
+  // Redireccionar (redirect) la navegación hacia [GET] /admin
+  res.rediret("admin");
+}
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {}
--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const pagesController = require("../controllers/pagesController");
 
 // Rutas relacionadas a la parte pública del sitio web:
 // ...
 const index = (req, res) => {
   res.render("home");
 };
+
+router.get("/", pagesController.showHome);
 
 module.exports = index;
