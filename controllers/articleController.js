9508a363511a0d66a36d64d13610ac6d10a04495
Gaston <gaston1silva@gmail.com>
2024-02-12 12:35:59 -0300

    NewArticle5

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -21,17 +21,29 @@ async function show(req, res) {
 
 // Show the form for creating a new resource
 async function create(req, res) {
-  // Esta vista tendrá un formulario que será el encargado de recibir los datos para crear
-  // un nuevo artículo.
-  // id = req.params.id
-  // const articles = await Article.findAll({});
-  res.render("create");
+  //creamos las vistas del newArticle
+
+  const users = await User.findAll();
+
+  res.render("newArticle", { users });
 }
 
 // Store a newly created resource in storage.
 async function store(req, res) {
   // - Recibir la información que proviene del formulario (req.body)
+  const { newTitle, newContent, newImage, newUserId } = req.body;
+
   // - Crear el artículo en la BBDD con todos los datos necesarios
+
+  await Article.create({
+    title: newTitle,
+    content: newContent,
+    image: newImage,
+    userId: newUserId,
+  });
+
+  // console.log("Jane's auto-generated ID:", jane.id);
+
   // - Redireccionar (redirect) la navegación hacia `[GET] /admin`
   res.redirect("admin");
 }
@@ -53,11 +65,13 @@ async function edit(req, res) {
 async function update(req, res) {
   //   - Recibir la información que proviene del formulario (req.body)
   const id = req.params.id;
-  const { newContent, newTitle } = req.body;
+  const { newContent, newTitle, newId } = req.body;
+
+  console.log(req.body);
 
   // - Hacer un update el artículo mencionado.
   await Article.update(
-    { title: newTitle, content: newContent },
+    { title: newTitle, content: newContent, userId: newId },
     {
       where: {
         id: id,
@@ -65,15 +79,21 @@ async function update(req, res) {
     },
   );
 
-  console.log(newContent);
-
   // Redireccionar (redirect) la navegación hacia [GET] /admin
   res.redirect("/admin");
 }
 
 // Remove the specified resource from storage.
-async function destroy(req, res) {}
+async function destroy(req, res) {
+  const id = req.params.id;
 
+  await Article.destroy({
+    where: {
+      id: id,
+    },
+  });
+  res.redirect("/admin");
+}
 // Otros handlers...
 // ...
 
