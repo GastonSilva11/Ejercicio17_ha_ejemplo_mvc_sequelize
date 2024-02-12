ed1d4c05d7d3647bd54b76233e7164acdd7d87cf
DViergutz <d.viergutz@hotmail.de>
2024-02-12 14:23:19 -0300

    crear comentario con ruta

---
+++ controllers/commentController.js
@@ -0,0 +1,24 @@
+const { Article, User, Comment } = require("../models");
+
+// Store a newly created resource in storage.
+async function store(req, res) {
+  const { userId, content, articleId } = req.body;
+  console.log(req.body);
+  await Comment.create({ userId, content, articleId });
+  res.redirect(`/articulos/${articleId}`);
+}
+
+module.exports = {
+  store,
+};
+
+// - `**[POST]` http://localhost:3000/comments** → Agregar comentario
+
+//     Esta ruta se encargará de:
+
+//     - Recibir la información que proviene del formulario de comentarios presente en la vista de detalle de artículos. (req.body)
+//     - Crear el comentario en la BBDD con todos los datos necesarios:
+//         - Nombre
+//         - Contenido
+//         - ID del artículo → Cómo podemos enviar este dato como parte del formulario? Pueden utilizar un input de tipo `hidden`: https://www.w3schools.com/tags/att_input_type_hidden.asp
+//     - Recargar la página
